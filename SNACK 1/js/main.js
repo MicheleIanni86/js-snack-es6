fc966609fb5c1605c2ac595d8bfb39e28ed93542
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-24 15:50:47 +0100

    feat: Completed Snack 1 and Created Folder for Snacks Exercises

--- SNACK 1/js/main.js
+++ SNACK 1/js/main.js
@@ -15,7 +15,21 @@ const vips = [
     'Maneskin',
 ];
 
-console.log(vips);
+
+const newListObj = vips.map((vip, numTavolo) => {
+
+    let newObject = {
+        nomeTavolo: 'Tavolo Vip',
+        vip: vip,
+        numTavolo: numTavolo + 1,
+    };
+
+    return newObject;
+
+});
+
+console.log(newListObj);
+
 
 
 
@@ -29,26 +43,3 @@ posto occupato.
 Generiamo e stampiamo in console l'array di oggetti per i segnaposto.*/
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------
-
-
-/*SNACK 2:
-Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame.
-Per preparare l'aula di un nuovo corso abbiamo bisogno dei seguenti dati:
-
-Un array di stringhe contenente il loro nome tutto in maiuscolo. ES (Marco della Rovere => MARCO DELLA ROVERE);
-
-Un array di oggetti "studente" che hanno un totale di voti superiore a 70
-
-Un array di oggetti "studente" che hanno un totale di voti superiore a 70 e id superiore a 120
-
-Questo è l'elenco degli studenti:
-| Id  | Name                  | Grades |
-| --- | --------------------- | ------ |
-| 213 | Marco della Rovere    | 78     |
-| 110 | Paola Cortellessa     | 96     |
-| 250 | Andrea Mantegna       | 48     |
-| 145 | Gaia Borromini        | 74     |
-| 196 | Luigi Grimaldello     | 68     |
-| 102 | Piero della Francesca | 50     |
-| 120 | Francesca da Polenta  | 84     |
-*/
