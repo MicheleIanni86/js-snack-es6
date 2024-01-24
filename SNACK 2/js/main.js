3d090eaf8e5343f48a426879f1365da840785a85
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-24 15:58:42 +0100

    feat: Created Array from DataTable

--- SNACK 2/js/main.js
+++ SNACK 2/js/main.js
@@ -1,5 +1,48 @@
 /*SNACK 2:
-Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame.
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame.*/
+
+
+const studenti = [
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grades: 78,
+    },
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grades: 96,
+    },
+    {
+        id: 250,
+        name: "Andrea Mantegna",
+        grades: 48,
+    },
+    {
+        id: 145,
+        name: "Gaia Borromini ",
+        grades: 74,
+    },
+    {
+        id: 196,
+        name: "Luigi Grimaldello",
+        grades: 68,
+    },
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50,
+    },
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84,
+    },
+];
+
+console.log(studenti);
+
+/*
 Per preparare l'aula di un nuovo corso abbiamo bisogno dei seguenti dati:
 
 Un array di stringhe contenente il loro nome tutto in maiuscolo. ES (Marco della Rovere => MARCO DELLA ROVERE);
