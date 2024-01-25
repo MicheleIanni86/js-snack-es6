ad999a574332afcb058817b020cc0b26557932fa
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-25 14:01:02 +0100

    feat: Add Object Array (snack 3)

---
+++ SNACK 3/js/main.js
@@ -0,0 +1,42 @@
+/*SNACK 3
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.*/
+const bikes = [
+    {
+        nome: 'Bici Rossa',
+        peso: 4,
+    },
+    {
+        nome: 'Bici Blu',
+        peso: 5,
+    },
+    {
+        nome: 'Bici Verde',
+        peso: 2,
+    },
+    {
+        nome: 'Bici Gialla',
+        peso: 7,
+    },
+    {
+        nome: 'Bici Fucsia',
+        peso: 6,
+    },
+    {
+        nome: 'Bici Nera',
+        peso: 9,
+    },
+    {
+        nome: 'Bici Marrone',
+        peso: 8,
+    },
+    {
+        nome: 'Bici Arancione',
+        peso: 6,
+    },
+];
+
+console.log(bikes);
+
+
+/*Stampare in console la bici con peso minore utilizzando destructuring e template literal*/
---
+++ SNACK 4/js/main.js
@@ -0,0 +1,44 @@
+/*SNACK 4
+Usa l'array allegato alla traccia per completare i seguenti punti:
+Valorizzare le proprietà dei punti fatti e dei falli subiti con numeri randomici
+Creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti, recuperandoli con il destructuring, e stampiamo tutto in console.
+BONUS
+Stampare in pagina oltre che in console!*/
+
+const teams = [
+    {
+        name: 'Team Turtle',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Frog',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Penguin',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Hippopotamus',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Seal',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Crocodile',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Dolphin',
+        score: 0,
+        foul: 0,
+    },
+];
