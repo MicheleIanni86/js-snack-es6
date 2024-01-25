64c5957e7e7d3b8e58dbba89ef116d965f1d9e04
MicheleIanni86 <micheleianni86@gmail.com>
2024-01-25 16:50:44 +0100

    feat: Change Valors in score e Foul SNACK 4

--- SNACK 4/js/main.js
+++ SNACK 4/js/main.js
@@ -1,9 +1,6 @@
 /*SNACK 4
 Usa l'array allegato alla traccia per completare i seguenti punti:
-Valorizzare le proprietà dei punti fatti e dei falli subiti con numeri randomici
-Creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti, recuperandoli con il destructuring, e stampiamo tutto in console.
-BONUS
-Stampare in pagina oltre che in console!*/
+Valorizzare le proprietà dei punti fatti e dei falli subiti con numeri randomici*/
 
 const teams = [
     {
@@ -42,3 +39,28 @@ const teams = [
         foul: 0,
     },
 ];
+
+
+teams.forEach((team) => {
+    team.score = randomNumbers(1, 100);
+    team.foul = randomNumbers(1, 10);
+
+});
+console.log(teams);
+
+
+
+/*Creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti, recuperandoli con il destructuring, e stampiamo tutto in console.
+BONUS
+Stampare in pagina oltre che in console!*/
+
+
+
+
+
+
+// FUNZIONI
+function randomNumbers(min, max) {
+    let numRand = Math.floor(Math.random() * (max - min + 1) + min);
+    return numRand;
+}
